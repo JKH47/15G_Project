b5b932d6159e18d2b09be12189064f124800ca1c
HyunHyoMin <159979121+HyunHyoMin@users.noreply.github.com>
2024-04-01 15:49:29 +0900

    add DB, create.html

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, redirect, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+DATABASE = 'database.db'
+
+def create_table():
+    conn = sqlite3.connect(DATABASE)
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            content TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        conn = sqlite3.connect(DATABASE)
+        cur = conn.cursor()
+        cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", (title, content))
+        conn.commit()
+        conn.close()
+        return redirect('/')
+    return render_template('create.html')
+
+if __name__ == '__main__':
+    create_table()
+    app.run(debug=True)
